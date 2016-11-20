eb8b7c4187bed3fa4b78e3d38754db59f7fc261c
Logan Smith <hamspamandeggs@gmail.com>
2016-11-20 20:49:42 +0000

    Not sure how to get this column onto the end

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,7 @@
--- SELECT f.title as film_title, c.name as customer_name, f.price as film_price, c.funds as customer_funds
--- FROM films f
--- INNER JOIN customers c
--- ON f.id = c.id;
+SELECT f.title as film_title, c.name as customer_name, f.price as film_price, c.funds as customer_funds
+FROM films f
+INNER JOIN customers c
+ON f.id = c.id;
 
 SELECT c.funds - f.price
 FROM customers c
